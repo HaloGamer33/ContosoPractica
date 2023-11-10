a71d893aaf3362d8731afb8f59d2930ff47c0aa0
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2023-11-10 15:23:47 -0600

    Practica 5

--- Migrations/20231110212233_Something.Designer.cs
+++ Migrations/20231110212233_Something.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ContosoUniversity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ContosoUniversity.Migrations
 {
     [DbContext(typeof(SchoolContextSQLite))]
-    partial class SchoolContextSQLiteModelSnapshot : ModelSnapshot
+    [Migration("20231110212233_Something")]
+    partial class Something
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.13");
